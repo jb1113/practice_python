1190d94c8f7b525164f3ba34a03459a8d0e18d83
jb1113 <leftydev1113@gmail.com>
2021-04-27 19:50:03 +0900

    string2

---
+++ string2.py
@@ -0,0 +1,26 @@
+# 문자열 연산하기
+
+# 문자열 더해서 연결하기(Concatenation)
+head = "Python"
+tail = " is fun!"
+head + tail
+# 'Python is fun!'
+
+# 문자열 곱하기
+str = "python"
+str * 2
+# 'pythonpython'
+
+# 문자열 곱하기 응용
+print("=" * 50)
+print("My Program")
+print("=" * 50)
+# ==================================================
+# My Program
+# ==================================================
+
+# 문자열 길이 구하기
+str = "Life is too short"
+len(str)
+# 17
+
